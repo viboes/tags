af6cd8f0967c298a3d462ab95f663ff31cfa293c
Vicente J. Botet Escriba <vicente.botet@wanadoo.fr>
2014-11-16 14:04:06 +0100

    Added tagged wrapper type.

---
+++ include/functional/v0/tags.hpp
@@ -0,0 +1,99 @@
+//////////////////////////////////////////////////////////////////////////////
+//
+// (C) Copyright Vicente J. Botet Escriba 2014.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file // LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+//////////////////////////////////////////////////////////////////////////////
+
+#ifndef FUNCTIONAL_V0_TAGS_HPP
+#define FUNCTIONAL_V0_TAGS_HPP
+
+#include <functional/v0/config.hpp>
+#include <meta/v0/types.hpp>
+
+#include <utility>
+
+namespace functional {
+  CONFIG_INLINE_NAMESPACE(v0) {
+
+    /// An absolute tag has associated two informations available at compile time
+    ///   * The type associated to a tag know at compile time. To do so the user must specialize the tag_type struct.
+    ///   * A unique index that can be used in ordered containers
+
+    template< class Tag >
+    struct tag_type;
+
+    template< class Tag >
+    using tag_type_t = typename tag_type<Tag>::type;
+
+#if 0
+    struct t1 {};
+    // specialization for t1
+    template<>
+    struct tag_type<t1> {
+      typedef int type;
+    };
+#endif
+
+    template< class Type, class Tag >
+    struct tagged
+    {
+        typedef Type value_type;
+        typedef Tag tag_type;
+
+        //tagged() = default;
+        //tagged(tagged const&) = default;
+        //tagged(tagged &&) = default;
+        //tagged& operator=(tagged const&) = default;
+        //tagged& operator=(tagged &&) = default;
+
+//        template <class T>
+//        tagged(T && v)
+//        : val(std::forward<T>(v)) {}
+
+        tagged(value_type const& v)
+        : val(v) {}
+
+        tagged(value_type && v)
+        : val(std::move(v)) {}
+
+        value_type& value() { return val; }
+        value_type const& value() const { return val; }
+        //value_type && value() && { return val; }
+
+    private:
+        value_type val;
+    };
+
+    template< class Tag, class Type >
+    tagged<tag_type_t<Tag>, Tag> make_tagged(Type && x)
+    {
+      return tagged<tag_type_t<Tag>, Tag>(std::forward<Type>(x));
+    }
+
+    template< class T>
+    struct value_type {
+      typedef T type;
+    };
+
+    template< class T, class Tag >
+    struct value_type<tagged<T, Tag> > {
+      typedef T type;
+    };
+
+    template< class T >
+    using value_type_t = typename value_type<T>::type;
+
+    template <class ...Tags>
+    struct tagged_types {
+      using type = meta::types<tagged<tag_type_t<Tags>, Tags> ...>;
+    };
+
+    template< class ...Tags >
+    using tags = typename tagged_types<Tags...>::type;
+
+  }
+}
+
+#endif // header
--- test/any/include_pass.cpp
+++ test/any/include_pass.cpp
@@ -8,10 +8,31 @@
 
 #include <data_type/v0/match_any.hpp>
 #include <functional/v0/match.hpp>
+#include <functional/v0/tags.hpp>
 
 #include <string>
 
 #include <boost/detail/lightweight_test.hpp>
+
+struct t1
+{};
+
+struct t2
+{};
+
+namespace functional
+{
+  template<>
+  struct tag_type<t1>
+  {
+    typedef int type;
+  };
+  template<>
+  struct tag_type<t2>
+  {
+    typedef std::string type;
+  };
+}
 
 int main()
 {
@@ -146,7 +167,7 @@ int main()
     // taking a list of accepted types for this argument.
     boost::any a = 2;
     boost::any b = 2;
-    functional::match_all<void>(std::make_tuple(select<types<int, std::string>>(a), select<types<int, std::string>>(a)),
+    functional::match_all<void>(std::make_tuple(select<types<int, std::string>>(a), select<types<int, std::string>>(b)),
         [](int const &i, int const &j )
         {
         },
@@ -160,30 +181,165 @@ int main()
         }
     );
   }
-
-#if 0
-  // compile fail as there is no overload for 'int'
   {
     boost::any a = 2;
-    boost::match<void>(a, types<int, std::string>{},
-        [](std::string& i)
+    boost::any b = std::string("2");
+    functional::match_all<void>(std::make_tuple(select<types<int, std::string>>(a), select<types<int, std::string>>(b)),
+        [](int i, int j )
+        {
+          BOOST_TEST(false);
+        },
+        [](auto const &i, auto const &j )
+        {
+        },
+        [](...)
         {
           BOOST_TEST(false);
         }
     );
   }
   {
-    // compile fail as there is no overload for cath all 'boost::any'
     boost::any a = 2;
-    boost::match<void>(a, types<std::string>{},
-        [](std::string& i)
+    boost::any b = std::string("2");
+    functional::match_all<void>(std::make_tuple(select<types<int, std::string>>(a), select<types<int, std::string>>(b)),
+        [](int i, int j )
+        {
+          BOOST_TEST(false);
+        },
+        [](auto &&i, auto &&j )
+        {
+        },
+        [](...)
+        {
+          BOOST_TEST(false);
+        }
+    );
+  }
+
+#if 0
+  // compile fails
+  {
+    boost::any a = 2;
+    boost::any b = std::string("2");
+    functional::match_all<void>(std::make_tuple(select<types<int, std::string>>(a), select<types<int, std::string>>(b)),
+        [](int i, int j )
+        {
+          BOOST_TEST(false);
+        },
+        [](...)
+        {
+        }
+    );
+  }
+#endif
+#if 0
+  // compile fail as there is no overload for 'int'
+  {
+    boost::any a = 2;
+    boost::match<void>(select<types<int, std::string>>(a)),
+        [](std::string const& i)
+        {
+          BOOST_TEST(false);
+        }
+    );
+  }
+  {
+    // compile fail as there is no overload for catch all 'boost::any'
+    boost::any a = 2;
+    boost::match<void>(select<types<std::string>>(a),
+        [](std::string const& i)
         {
           BOOST_TEST(false);
         }
     );
   }
 #endif
+  {
+    // functional::match functional::select<types<>>(a)
+    // taking a list of accepted types for this argument.
+    boost::any a = make_tagged<t2>("1");
+    functional::match<void>(functional::select<types<tagged<int,t1>>>(a),
+        [](tagged<int, t1> const&i)
+        {
+          BOOST_TEST(false);
+        },
+        [](...)
+        {
+
+        }
+    );
+  }
+  {
+    // functional::match functional::select<types<>>(a)
+    // taking a list of accepted types for this argument.
+    boost::any a = make_tagged<t1>(1);
+    functional::match<void>(functional::select<functional::tags<t1>>(a),
+        [](tagged<int, t1> i)
+        {
+        },
+        [](...)
+        {
+          BOOST_TEST(false);
+
+        }
+    );
+  }
+  {
+    boost::any ta = make_tagged<t1>(1);
+    boost::any tb = make_tagged<t2>(std::string("2"));
+    match_all<void>(std::make_tuple(select<functional::tags<t1, t2>>(ta), select<functional::tags<t1, t2>>(tb)),
+        [](tagged<int, t1>           const&      i,  tagged<int, t1>         const&        j)
+        {
+          BOOST_TEST(false);
+        },
+        [](tagged<int, t1>           const&      i,  tagged<std::string, t2>  const& j)
+        {
+
+        },
+        [](tagged<std::string, t2>  const& i,  tagged<int, t1>         const&        j)
+        {
+          BOOST_TEST(false);
+        },
+        [](tagged<std::string, t2>  const& i,  tagged<std::string, t2>  const& j)
+        {
+          BOOST_TEST(false);
+        },
+        [](auto const &i, auto const &j )
+        {
+          BOOST_TEST(false);
+        },
+        [](...)
+        {
+          BOOST_TEST(false);
+        }
+    );
+  }
+  {
+    boost::any ta = make_tagged<t1>(1);
+    boost::any tb = make_tagged<t2>(std::string("2"));
+    match_all<void>(std::make_tuple(select<functional::tags<t1, t2>>(ta), select<functional::tags<t1, t2>>(tb)),
+        [](tagged<int, t1>           const&      i,  tagged<int, t1>         const&        j)
+        {
+          BOOST_TEST(false);
+        },
+        [](tagged<std::string, t2>  const& i,  tagged<int, t1>         const&        j)
+        {
+          BOOST_TEST(false);
+        },
+        [](tagged<std::string, t2>  const& i,  tagged<std::string, t2>  const& j)
+        {
+          BOOST_TEST(false);
+        },
+        [](auto const &i, auto const &j )
+        {
+        },
+        [](...)
+        {
+          BOOST_TEST(false);
+        }
+    );
+  }
+
   return boost::report_errors();
 }
-
 
